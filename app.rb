0f6118b3b80aeb84022aa671314c5b276a5fb5ba
Josh <joshua.ng2332@gmail.com>
2020-06-23 15:59:36 +0100

    Intro to web afternoon project. Progress: 'Sinatra Using forms'

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  'Hello!'
+end
+
+get '/secret' do
+  'This is a secret'
+end
+
+get '/game' do
+  'This is a cool game'
+end
+
+get '/hello_world' do
+  'Hello World'
+end
+
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
